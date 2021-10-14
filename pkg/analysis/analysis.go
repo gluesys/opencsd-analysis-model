f85dc0b592548445a3c11f0fbf421d62c4282cb7
ChanHyupKim <atyx300@naver.com>
2021-10-14 10:29:29 +0900

    printMemUsage update

--- pkg/analysis/analysis.go
+++ pkg/analysis/analysis.go
@@ -112,7 +112,6 @@ func GetMemory() {
 func PrintMemUsage() {
 	var m runtime.MemStats
 	runtime.ReadMemStats(&m)
-	// For info on each, see: https://golang.org/pkg/runtime/#MemStats
 	fmt.Printf("Alloc = %v MiB", bToMb(m.Alloc))
 	fmt.Printf("\tTotalAlloc = %v MiB", bToMb(m.TotalAlloc))
 	fmt.Printf("\tSys = %v MiB", bToMb(m.Sys))
