5d1e9c82efefe16b0b6a5f729f49b2194bc46a45
ChanHyupKim <atyx300@naver.com>
2021-10-14 11:06:54 +0900

    main update

--- cmd/main.go
+++ cmd/main.go
@@ -5,45 +5,6 @@ import (
 	"log"
 )
 
-// func main() {
-// 	log.SetFlags(log.Lshortfile)
-// 	per, _ := cpu2.Percent(time.Second, false)
-// 	log.Println(per)
-
-// 	cpu, err := cpu1.Get()
-// 	if err != nil {
-// 		log.Println("Fail")
-// 		return
-// 	}
-// 	log.Println(cpu.Total)
-
-// }
-
-// func getCPUSample() (idle, total uint64) {
-// 	contents, err := ioutil.ReadFile("/proc/stat")
-// 	if err != nil {
-// 		return
-// 	}
-// 	lines := strings.Split(string(contents), "\n")
-// 	for _, line := range lines {
-// 		fields := strings.Fields(line)
-// 		if fields[0] == "cpu" {
-// 			numFields := len(fields)
-// 			for i := 1; i < numFields; i++ {
-// 				val, err := strconv.ParseUint(fields[i], 10, 64)
-// 				if err != nil {
-// 					fmt.Println("Error: ", i, fields[i], err)
-// 				}
-// 				total += val // tally up all the numbers to get total ticks
-// 				if i == 4 {  // idle is the 5th field in the cpu line
-// 					idle = val
-// 				}
-// 			}
-// 			return
-// 		}
-// 	}
-// 	return
-// }
 func main() {
 	log.SetFlags(log.Lshortfile)
 	log.Println("50500 Server Start")
