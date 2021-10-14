c8e3c74ac16f3204b7caf9f4f35b55e9fb00a305
KIMCOYG <kjhkjh9753@naver.com>
2021-10-14 13:22:42 +0900

    main update & Request Parse submodule dev

--- simulator/go.mod
+++ simulator/go.mod
@@ -1,3 +1,8 @@
 module simulator
 
 go 1.16
+
+require (
+	github.com/mackerelio/go-osstat v0.2.1
+	github.com/olekukonko/tablewriter v0.0.5
+)
--- simulator/simulation.go
+++ simulator/simulation.go
@@ -1,7 +1,16 @@
 package simulator
 
 import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"simulator/analysis"
 	types "simulator/type"
+	"strconv"
+	"strings"
+
+	"github.com/olekukonko/tablewriter"
 )
 
 type Snippet struct {
@@ -63,4 +72,82 @@ type ResponseA struct {
 	Code    int         `json:"code"`
 	Message string      `json:"message"`
 	Data    interface{} `json:"data"`
+}
+
+var flag = 1
+var ans analysis.Analysis
+
+const rootDirectory = "/root/workspace/usr/coyg/module/tpch/"
+
+func resJsonParser(jsonDataString string) Response {
+	var res Response
+
+	if err := json.Unmarshal([]byte(jsonDataString), &res); err != nil {
+		log.Fatal(err)
+	}
+
+	return res
+}
+
+func isAggregateFunc(atom string) (bool, string) {
+	aggregateList := []string{"count", "sum", "avg", "max", "min"}
+	for _, aggregater := range aggregateList {
+		if strings.Contains(atom, aggregater+"(") {
+			return true, aggregater
+		}
+	}
+	return false, ""
+}
+func isOperator(atom string) (bool, string) {
+	opList := []string{"and", "or"}
+	for _, op := range opList {
+		atom = strings.ToLower(atom)
+		if atom == op {
+			return true, op
+		}
+	}
+	return false, ""
+}
+func isEXP(atom string) (bool, string) {
+	expList := []string{">=", "<=", ">", "<", "="}
+	for _, exp := range expList {
+		if strings.Contains(atom, exp) {
+			return true, exp
+		}
+	}
+	return false, ""
+}
+func printClient(res Response) {
+	if res.Code == 200 {
+
+		datas := [][]string{}
+
+		for _, value := range res.Data.Values {
+			data := []string{}
+			for _, field := range res.Data.Field {
+				data = append(data, string(value[field]))
+			}
+
+			datas = append(datas, data)
+
+		}
+
+		fmt.Println()
+
+		table := tablewriter.NewWriter(os.Stdout)
+
+		table.SetHeader(res.Data.Field)
+		table.SetBorder(true)
+		table.SetAutoFormatHeaders(false)
+		table.SetCaption(true, "Total: "+strconv.Itoa(len(datas)))
+		table.AppendBulk(datas)
+		table.Render()
+
+	} else {
+		log.Fatal(res.Message)
+	}
+}
+
+func main() {
+	log.SetFlags(log.Lshortfile)
 }
