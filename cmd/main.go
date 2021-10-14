dd65c0fc551e87479191903fda5000eeb5d748c3
ChanHyupKim <atyx300@naver.com>
2021-10-14 10:29:03 +0900

    main update

--- cmd/main.go
+++ cmd/main.go
@@ -47,15 +47,6 @@ import (
 func main() {
 	log.SetFlags(log.Lshortfile)
 	log.Println("50500 Server Start")
-
-	// idle0, total0 := getCPUSample()
-	// time.Sleep(3 * time.Second)
-	// idle1, total1 := getCPUSample()
-	// idleTicks := float64(idle1 - idle0)
-	// totalTicks := float64(total1 - total0)
-	// cpuUsage := 100 * (totalTicks - idleTicks) / totalTicks
-	// fmt.Printf("CPU usage is %f%% [busy: %f, total: %f]\n", cpuUsage, totalTicks-idleTicks, totalTicks)
-	// go rest.Run()
 	rest.Run()
 
 }
